1a0dab282114607bb026db1e542dc7da71a307a6
Halil CAN <halilc.2001@gmail.com>
2021-09-15 17:49:20 +0300

    added backend actions

---
+++ backend/app/Http/Controllers/API/Auth/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function handle()
+    {
+        return "ok";
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//versiyon ??
+
+Route::post('register', [RegisterController::class, 'handle']);
+
+Route::get('test', function () {
+    return 'ok';
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
