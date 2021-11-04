8efe2bd9856ef583cf88fc2e3d48f6df17df57f8
Halil CAN <halilc.2001@gmail.com>
2021-11-04 21:08:57 +0300

    worked on votes list

--- frontend/src/components/dashboard/team/home/VotesList.vue
+++ frontend/src/components/dashboard/team/home/VotesList.vue
@@ -29,25 +29,28 @@
               <div v-for="(vote,index) in votes.active"
                    :key="index"
                    :class="{voted:vote.is_voted,'everyone-voted':vote.voted_percentage === 100}"
-                   class="item">
-                <div class="top">
-                  <div class="voted-percentage">
-                    %{{ vote.voted_percentage }}
+                   class="item-container">
+                <!-- item -->
+                <div class="item">
+                  <div class="top">
+                    <div class="voted-percentage">
+                      %{{ vote.voted_percentage }}
+                    </div>
+                    <div class="icon">
+                      <img :src="require(`../../../../assets/icons/${vote.type}-type-vote.png`)" alt="vote-type"/>
+                    </div>
+                    <div class="vote-name">
+                      {{ vote.title }}
+                    </div>
                   </div>
-                  <div class="icon">
-                    <img :src="require(`../../../../assets/icons/${vote.type}-type-vote.png`)" alt="vote-type"/>
+                  <div class="bottom-info time">
+                    <i class="bi bi-stopwatch-fill"></i>
+                    {{ $dayjs($helpers.convertTimeToUtc(vote.end_date)).fromNow() }}
                   </div>
-                  <div class="vote-name">
-                    {{ vote.title }}
+                  <div class="bottom-info voted-info">
+                    <i class="bi bi-check-all"></i>
+                    Oy Verilmiş !
                   </div>
-                </div>
-                <div class="bottom-info time">
-                  <i class="bi bi-stopwatch-fill"></i>
-                  {{ $dayjs($helpers.convertTimeToUtc(vote.end_date)).fromNow() }}
-                </div>
-                <div class="bottom-info voted-info">
-                  <i class="bi bi-check-all"></i>
-                  Oy Verilmiş !
                 </div>
               </div>
             </template>
@@ -191,10 +194,17 @@ export default {
         // TODO: Problem
         position: relative;
         height: 100%;
+
         display: flex;
         flex-wrap: wrap;
         justify-content: flex-start;
-        gap: 30px;
+        width: 100%;
+
+
+        &::after {
+          content: "";
+          flex: auto;
+        }
 
         .no-data {
           margin-top: 30px;
@@ -215,52 +225,66 @@ export default {
           }
         }
 
-        .item {
-          width: 130px;
-          // min-width: 120px;
-          // width: 20%;
-          margin: 12px 0;
-          cursor: pointer;
-          border-radius: 5px;
-          transition: .2s;
+        .item-container {
+          display: flex;
+          justify-content: center;
+          width: 25%;
+          margin: 10px 0;
 
-          .top {
-            background-color: white;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding: 15px 7px;
-            border: 1px solid #eeeeee;
-            border-bottom: 0;
-            border-radius: 5px 5px 0 0;
-            transition: .3s;
+          &:nth-child(4n+1) {
+            //!important ?
+            justify-content: flex-start;
+          }
 
-            .icon {
-              img {
-                width: 45px;
+          &:nth-child(4n) {
+            justify-content: flex-end;
+          }
+
+          .item {
+            min-width: 130px;
+            cursor: pointer;
+            border-radius: 5px;
+            transition: .2s;
+
+
+            .top {
+              background-color: white;
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              padding: 15px 7px;
+              border: 1px solid #eeeeee;
+              border-bottom: 0;
+              border-radius: 5px 5px 0 0;
+              transition: .3s;
+
+              .icon {
+                img {
+                  width: 45px;
+                }
+              }
+
+              .vote-name {
+                width: 100%;
+                height: 100%;
+                font-size: 13px;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                overflow: hidden;
+                text-align: center;
               }
             }
 
-            .vote-name {
-              width: 100%;
-              height: 100%;
-              font-size: 13px;
-              text-overflow: ellipsis;
-              white-space: nowrap;
-              overflow: hidden;
+            .bottom-info {
+              padding: 3px;
               text-align: center;
-            }
-          }
+              width: 100%;
+              font-size: 12px;
+              border-radius: 0 0 5px 5px;
 
-          .bottom-info {
-            padding: 3px;
-            text-align: center;
-            width: 100%;
-            font-size: 12px;
-            border-radius: 0 0 5px 5px;
-
-            &.voted-info {
-              display: none;
+              &.voted-info {
+                display: none;
+              }
             }
           }
         }
@@ -269,71 +293,73 @@ export default {
 
     .active-votes {
       .list {
-        .item {
-          &:hover .top {
-            background-color: #f2f6ff;
-            border-color: $df-blue-color;
-          }
+        .item-container {
+          .item {
+            &:hover .top {
+              background-color: #f2f6ff;
+              border-color: $df-blue-color;
+            }
 
-          &.voted {
-            cursor: default;
+            &.voted {
+              cursor: default;
+
+              .top {
+                background-color: #f5fffb;
+                border-color: $df-green-color;
+
+                .vote-name {
+                  color: #888888;
+                }
+              }
+
+              .bottom-info {
+                background-color: $df-green-color;
+
+                &.time {
+                  display: none;
+                }
+
+                &.voted-info {
+                  display: block;
+                }
+              }
+            }
+
+            &.everyone-voted {
+              .top {
+                .voted-percentage {
+                  background-color: $df-green-color;
+                  color: white;
+
+                }
+              }
+            }
 
             .top {
-              background-color: #f5fffb;
-              border-color: $df-green-color;
+              position: relative;
+
+              .voted-percentage {
+                font-size: 10px;
+                position: absolute;
+                left: 4px;
+                top: 4px;
+                color: $df-mdl-dark-black-color;
+                background-color: #eeeeee;
+                // background-color: $df-green-color;
+                // color: white;
+                padding: 2px 5px;
+                border-radius: 20px;
+              }
 
               .vote-name {
-                color: #888888;
+                color: $df-blue-color;
               }
             }
 
             .bottom-info {
-              background-color: $df-green-color;
-
-              &.time {
-                display: none;
-              }
-
-              &.voted-info {
-                display: block;
-              }
+              background-color: $df-blue-color;
+              color: white;
             }
-          }
-
-          &.everyone-voted {
-            .top {
-              .voted-percentage {
-                background-color: $df-green-color;
-                color: white;
-
-              }
-            }
-          }
-
-          .top {
-            position: relative;
-
-            .voted-percentage {
-              font-size: 10px;
-              position: absolute;
-              left: 4px;
-              top: 4px;
-              color: $df-mdl-dark-black-color;
-              background-color: #eeeeee;
-              // background-color: $df-green-color;
-              // color: white;
-              padding: 2px 5px;
-              border-radius: 20px;
-            }
-
-            .vote-name {
-              color: $df-blue-color;
-            }
-          }
-
-          .bottom-info {
-            background-color: $df-blue-color;
-            color: white;
           }
         }
       }
@@ -351,23 +377,79 @@ export default {
       }
 
       .list {
-        .item {
-          cursor: default;
+        .item-container {
+          .item {
+            cursor: default;
 
-          &:hover .top {
-            border-color: #dadada;
-            background-color: #f8f8f8;
-          }
+            &:hover .top {
+              border-color: #dadada;
+              background-color: #f8f8f8;
+            }
 
-          .top {
-            .vote-name {
+            .top {
+              .vote-name {
+                color: $df-mdl-dark-black-color;
+              }
+            }
+
+            .bottom-info {
               color: $df-mdl-dark-black-color;
+              background-color: #dadada;
             }
           }
+        }
+      }
+    }
+  }
+}
 
-          .bottom-info {
-            color: $df-mdl-dark-black-color;
-            background-color: #dadada;
+@media only screen and (max-width: 1100px) {
+  .votes {
+    .list-container {
+      .votes-type-list {
+        .list {
+
+          .item-container {
+            background-color: blue;
+            width: 50% !important;
+
+            &:nth-child(2n+1) {
+              justify-content: flex-start;
+            }
+
+            &:nth-child(2n+2) {
+              justify-content: flex-end;
+            }
+
+            .item {
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+@media only screen and (max-width: 1312px) {
+  .votes {
+    .list-container {
+      .votes-type-list {
+        .list {
+          .item-container {
+            width: 33% !important;
+
+            &:nth-child(3n+1) {
+              justify-content: flex-start;
+            }
+
+            &:nth-child(3n+3) {
+              justify-content: flex-end;
+            }
+
+
+
+            .item {
+            }
           }
         }
       }
