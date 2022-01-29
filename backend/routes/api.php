0fa22f63cf9a344381b696a4e7c457d9668eac0e
Halil CAN <halilc.2001@gmail.com>
2022-01-29 12:01:25 +0300

    update api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -38,58 +38,72 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//TODO: Routes
-//TODO: team:key olayını her yerde yazmak yerine, tek bir yerde tanımlamak?
-
 Route::get('version', VersionController::class);
 
 Route::group(['prefix' => 'v1'], function () {
+    // Register
     Route::post('register', [RegisterController::class, 'handle']);
     Route::post('register/email/send', [RegisterController::class, 'sendEmail']);
 
+    // Login
     Route::post('login', [LoginController::class, 'handle']);
     Route::resource('forgot-password', ForgotPasswordController::class)->scoped(['forgot_password' => 'key']);
 
-
     Route::group(['middleware' => 'auth:sanctum'], function () {
+        // User
         Route::put('user/language', UserLanguageController::class);
         Route::put('user/settings', [UserSettingsController::class, 'update']);
         Route::resource('user/notifications', UserNotificationController::class);
 
+        // File
         Route::resource('file', FileController::class);
 
+        // Team
         Route::resource('teams', TeamController::class);
-        Route::get('teams/{team:key}/check-user', TeamCheckUserController::class);
-        Route::get('teams/{team:key}/settings', [TeamSettingsController::class, 'index']);
-        Route::put('teams/{team:key}/settings', [TeamSettingsController::class, 'update']);
-        Route::get('teams/{team:key}/info', TeamInfoController::class);
+        Route::group(['prefix' => 'teams/{team:key}/'], function () {
+            Route::get('check-user', TeamCheckUserController::class);
+            Route::get('settings', [TeamSettingsController::class, 'index']);
+            Route::put('settings', [TeamSettingsController::class, 'update']);
+            Route::get('info', TeamInfoController::class);
+        });
 
+        // Vote Of Team
         Route::resource('teams.votes', VoteController::class)->scoped(['team' => 'key']);
+
+        // Power Vote Of Team
         Route::group(['prefix' => 'teams/{team:key}/vote-types/power/'], function () {
             Route::get('check-vote', [VotePowerTypeActionsController::class, 'checkVote']);
             Route::get('check-store', [VotePowerTypeActionsController::class, 'checkStore']);
             Route::get('check-time', [VotePowerTypeActionsController::class, 'checkTime']);
         });
 
+        // Finished Vote Of Team
         Route::resource('teams.finished-votes', FinishedVoteController::class)->scoped(['team' => 'key']);
 
+        // Team Notification
         Route::get('teams/{team:key}/notifications', TeamNotificationController::class);
 
-        //TODO:bunu düzelt! Resource contoller controller içine alınacak !
-        Route::delete('teams/{team:key}/users/me', TeamUserLeaveFromTeamController::class);
-        Route::delete('teams/{team:key}/users/{team_user}', [TeamUserController::class, 'destroy']); //? altla aynı ?
+        // Team User
+        Route::group(['prefix' => 'teams/{team:key}/users/'], function () {
+            Route::delete('me', TeamUserLeaveFromTeamController::class);
+            Route::delete('{team_user}', [TeamUserController::class, 'destroy']);
+        });
         Route::resource('teams.users', TeamUserController::class)->scoped(['team' => 'key']);
 
+        // Team Permission Of User
         Route::get('teams/{team:key}/my-permissions', TeamUserPermissionController::class);
 
-        //TODO: Review code
+        // Voted User
         Route::post('votes/{vote}/voted-users', [VotedUserController::class, 'store']);
 
+        // Join Team
         Route::post('team/join/team-code', TeamJoinWithCodeController::class);
         Route::post('team/join/user-invitation', TeamJoinWithInvitationController::class);
 
-        Route::resource('teams.invitations', TeamUserInvitationController::class)->scoped(['team' => 'key']);;;
+        // Team Invitation
+        Route::resource('teams.invitations', TeamUserInvitationController::class)->scoped(['team' => 'key']);
 
+        // Logout
         Route::post('logout', [LogoutController::class, 'handle']);
     });
 });
